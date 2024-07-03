5de70984c82ad31e12700764677926683b7e3d21
Lekha4Pagadala <lekhapagadala6@gmail.com>
2024-07-03 14:17:37 +0530

    chapter5 of react

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+// abpout exort and imports written in README.MD file 
+//in a file we cannot do 2 exports only one we can :: by default it can export one
+//suppose we have to export multiple then we should do as named export  like writing export keyword behind this
+//how do we import these 2 named export in the files like below in the necessary file
+//import { CDN_URL } from "../utils/constants";
+
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
+
+export const LOGO_URL="https://mir-s3-cdn-cf.behance.net/project_modules/hd/25539c29532269.55f7d6a0a8c71.jpg";
+
---
+++ src/utils/mockData.js
@@ -0,0 +1,611 @@
+const resList=[
+    {
+    id: "93741",
+    name: "Subway",
+    cloudinaryImageId: "63178e3e64d503a479f2a2048a474552",
+    locality: "The Nexus Mall",
+    areaName: "Koramangala",
+    costForTwo: "₹350 for two",
+    cuisines: [
+    "Salads",
+    "Snacks",
+    "Desserts",
+    "Beverages"
+    ],
+    avgRating: 4,
+    parentId: "2",
+    avgRatingString: "4.0",
+    totalRatingsString: "5K+",
+    sla: {
+    deliveryTime: 37,
+    lastMileTravel: 1.6,
+    serviceability: "SERVICEABLE",
+    slaString: "35-40 mins",
+    lastMileTravelString: "1.6 km",
+    iconType: "ICON_TYPE_EMPTY"
+    },
+    availability: {
+    nextCloseTime: "2024-06-10 05:45:00",
+    opened: true
+    },
+    badges: {
+    imageBadges: [
+    {
+    imageId: "Rxawards/_CATEGORY-Sandwiches.png",
+    description: "Delivery!"
+    }
+    ]
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+    entityBadges: {
+    imageBased: {
+    badgeObject: [
+    {
+    attributes: {
+    description: "Delivery!",
+    imageId: "Rxawards/_CATEGORY-Sandwiches.png"
+    }
+    }
+    ]
+    },
+    textBased: { },
+    textExtendedBadges: { }
+    }
+    },
+    aggregatedDiscountInfoV3: {
+    header: "60% OFF",
+    subHeader: "UPTO ₹120"
+    },
+    differentiatedUi: {
+    displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    differentiatedUiMediaDetails: {
+    mediaType: "ADS_MEDIA_ENUM_IMAGE",
+    lottie: { },
+    video: { }
+    }
+    },
+    reviewsSummary: { },
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: { }
+},
+{
+    id: "70246",
+    name: "Starbucks Coffee",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/3/03f89277-010b-4240-a654-24faa97649f0_70246.JPG",
+    locality: "Koramangala",
+    areaName: "Koramangala",
+    costForTwo: "₹400 for two",
+    cuisines: [
+    "Beverages",
+    "Cafe",
+    "Snacks",
+    "Desserts",
+    "Bakery",
+    "Ice Cream"
+    ],
+    avgRating: 4.4,
+    parentId: "195515",
+    avgRatingString: "4.4",
+    totalRatingsString: "5K+",
+    sla: {
+    deliveryTime: 21,
+    lastMileTravel: 1.4,
+    serviceability: "SERVICEABLE",
+    slaString: "20-25 mins",
+    lastMileTravelString: "1.4 km",
+    iconType: "ICON_TYPE_EMPTY"
+    },
+    availability: {
+    nextCloseTime: "2024-06-10 23:59:00",
+    opened: true
+    },
+    badges: {
+    textExtendedBadges: [
+    {
+    iconId: "guiltfree/GF_Logo_android_3x",
+    shortDescription: "options available",
+    fontColor: "#7E808C"
+    }
+    ]
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+    entityBadges: {
+    imageBased: { },
+    textBased: { },
+    textExtendedBadges: {
+    badgeObject: [
+    {
+    attributes: {
+    description: "",
+    fontColor: "#7E808C",
+    iconId: "guiltfree/GF_Logo_android_3x",
+    shortDescription: "options available"
+    }
+    }
+    ]
+    }
+    }
+    },
+    aggregatedDiscountInfoV3: {
+    header: "20% OFF",
+    subHeader: "UPTO ₹50"
+    },
+    differentiatedUi: {
+    displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    differentiatedUiMediaDetails: {
+    mediaType: "ADS_MEDIA_ENUM_IMAGE",
+    lottie: { },
+    video: { }
+    }
+    },
+    reviewsSummary: { },
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: { } 
+},
+{
+    id: "319086",
+name: "Baskin Robbins - Ice Cream Desserts",
+cloudinaryImageId: "85ccae4e3576f9330af102c46ca85395",
+locality: "4th B Block",
+areaName: "Koramangala",
+costForTwo: "₹250 for two",
+cuisines: [
+"Desserts"
+],
+avgRating: 4.6,
+veg: true,
+parentId: "5588",
+avgRatingString: "4.6",
+totalRatingsString: "1K+",
+sla: {
+deliveryTime: 28,
+lastMileTravel: 0.6,
+serviceability: "SERVICEABLE",
+slaString: "25-30 mins",
+lastMileTravelString: "0.6 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2024-06-09 23:00:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "v1695133679/badges/Pure_Veg111.png",
+description: "pureveg"
+}
+],
+textExtendedBadges: [
+{
+iconId: "Ratnesh_Badges/test2.png",
+shortDescription: "Perfect ice cream delivery",
+fontColor: "#7E808C"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "pureveg",
+imageId: "v1695133679/badges/Pure_Veg111.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: {
+badgeObject: [
+{
+attributes: {
+description: "",
+fontColor: "#7E808C",
+iconId: "Ratnesh_Badges/test2.png",
+shortDescription: "Perfect ice cream delivery"
+}
+}
+]
+}
+}
+},
+aggregatedDiscountInfoV3: {
+header: "50% OFF",
+subHeader: "UPTO ₹100"
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { }
+},
+    {
+    id: "25620",
+    name: "KFC",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/69e69c55-d2bb-4207-8310-131fc693f78a_25620.JPG",
+    locality: "Intermediate Ring Road",
+    areaName: "Ejipura",
+    costForTwo: "₹400 for two",
+    cuisines: [
+    "Burgers",
+    "Fast Food",
+    "Rolls & Wraps"
+    ],
+    avgRating: 4.2,
+    parentId: "547",
+    avgRatingString: "4.2",
+    totalRatingsString: "10K+",
+    sla: {
+    deliveryTime: 29,
+    lastMileTravel: 0.8,
+    serviceability: "SERVICEABLE",
+    slaString: "25-30 mins",
+    lastMileTravelString: "0.8 km",
+    iconType: "ICON_TYPE_EMPTY"
+    },
+    availability: {
+    nextCloseTime: "2024-06-09 23:00:00",
+    opened: true
+    },
+    badges: { },
+    isOpen: true,
+    aggregatedDiscountInfoV2: { },
+    type: "F",
+    badgesV2: {
+    entityBadges: {
+    imageBased: { },
+    textBased: { },
+    textExtendedBadges: { }
+    }
+    },
+    differentiatedUi: {
+    displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    differentiatedUiMediaDetails: {
+    mediaType: "ADS_MEDIA_ENUM_IMAGE",
+    lottie: { },
+    video: { }
+    }
+    },
+    reviewsSummary: { },
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: { }
+    
+},
+{
+    id: "23678",
+name: "McDonald's",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/1fe8039a-0a1a-41f8-8db1-f3521ba8fada_23678.jpg",
+locality: "5th Block",
+areaName: "Koramangala",
+costForTwo: "₹400 for two",
+cuisines: [
+"Burgers",
+"Beverages",
+"Cafe",
+"Desserts"
+],
+avgRating: 4.5,
+parentId: "630",
+avgRatingString: "4.5",
+totalRatingsString: "10K+",
+sla: {
+deliveryTime: 30,
+lastMileTravel: 1.2,
+serviceability: "SERVICEABLE",
+slaString: "25-30 mins",
+lastMileTravelString: "1.2 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2024-06-09 23:59:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "Rxawards/_CATEGORY-Burger.png",
+description: "Delivery!"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "Delivery!",
+imageId: "Rxawards/_CATEGORY-Burger.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "40% OFF",
+subHeader: "UPTO ₹80"
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { }
+},
+{
+    id: "381853",
+name: "ITC Master Chef Creations",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/21/056aad5c-08e5-4fba-9037-850bc6d37055_381853.JPG",
+locality: "Koramangala",
+areaName: "Koramangala",
+costForTwo: "₹650 for two",
+cuisines: [
+"North Indian",
+"Biryani",
+"Kebabs",
+"Mediterranean",
+"Mughlai"
+],
+avgRating: 4.4,
+parentId: "11992",
+avgRatingString: "4.4",
+totalRatingsString: "1K+",
+sla: {
+deliveryTime: 39,
+lastMileTravel: 1.4,
+serviceability: "SERVICEABLE",
+slaString: "35-40 mins",
+lastMileTravelString: "1.4 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2024-06-10 01:00:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "newg.png",
+description: "Gourmet"
+}
+],
+textExtendedBadges: [
+{
+iconId: "guiltfree/GF_Logo_android_3x",
+shortDescription: "options available",
+fontColor: "#7E808C"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "Gourmet",
+imageId: "newg.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: {
+badgeObject: [
+{
+attributes: {
+description: "",
+fontColor: "#7E808C",
+iconId: "guiltfree/GF_Logo_android_3x",
+shortDescription: "options available"
+}
+}
+]
+}
+}
+},
+aggregatedDiscountInfoV3: {
+header: "50% OFF",
+subHeader: "UPTO ₹100"
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { }
+},{
+    id: "158855",
+name: "Candice's Gourmet Sandwiches & Wraps",
+cloudinaryImageId: "d1331113564b015c9d55c48ff48a2553",
+locality: "5th Block",
+areaName: "Koramangala",
+costForTwo: "₹600 for two",
+cuisines: [
+"Continental",
+"American",
+"Salads",
+"Desserts",
+"Beverages",
+"Healthy Food"
+],
+avgRating: 4.5,
+parentId: "465403",
+avgRatingString: "4.5",
+totalRatingsString: "1K+",
+sla: {
+deliveryTime: 36,
+lastMileTravel: 0.4,
+serviceability: "SERVICEABLE",
+slaString: "35-40 mins",
+lastMileTravelString: "0.4 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2024-06-09 23:00:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "newg.png",
+description: "Gourmet"
+}
+],
+textExtendedBadges: [
+{
+iconId: "guiltfree/GF_Logo_android_3x",
+shortDescription: "options available",
+fontColor: "#7E808C"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "Gourmet",
+imageId: "newg.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: {
+badgeObject: [
+{
+attributes: {
+description: "",
+fontColor: "#7E808C",
+iconId: "guiltfree/GF_Logo_android_3x",
+shortDescription: "options available"
+}
+}
+]
+}
+}
+},
+aggregatedDiscountInfoV3: {
+header: "50% OFF",
+subHeader: "UPTO ₹100"
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { }
+},
+{
+    id: "41913",
+name: "Chinita Real Mexican Food",
+cloudinaryImageId: "29fc772cc06b07a4b018e4971d96c9a3",
+locality: "Koramangala",
+areaName: "Koramangala",
+costForTwo: "₹750 for two",
+cuisines: [
+"Mexican",
+"Salads",
+"Desserts",
+"Beverages"
+],
+avgRating: 4.5,
+parentId: "5064",
+avgRatingString: "4.5",
+totalRatingsString: "5K+",
+sla: {
+deliveryTime: 30,
+lastMileTravel: 0.4,
+serviceability: "SERVICEABLE",
+slaString: "30-35 mins",
+lastMileTravelString: "0.4 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2024-06-09 23:00:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "newg.png",
+description: "Gourmet"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "Gourmet",
+imageId: "newg.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "50% OFF",
+subHeader: "UPTO ₹100"
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { }
+}];
+
+export default resList;
